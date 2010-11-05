3162c683c1cdcea9b1145fc069087abba381e012
Brandon Keepers <brandon@collectiveidea.com>
2010-11-05 10:41:42 -0400

    whitespace

--- lib/delayed/backend/mongoid.rb
+++ lib/delayed/backend/mongoid.rb
@@ -13,12 +13,11 @@ module Delayed
         field :locked_by,   :type=> String
         field :failed_at,   :type=> Time
         field :last_error,  :type=> String
-      
-      
+
         index :locked_by
 
         before_save :set_default_run_at
-        
+
         def self.before_fork
           ::Mongoid.master.connection.close
         end
@@ -26,19 +25,19 @@ module Delayed
         def self.after_fork
           ::Mongoid.master.connection.connect
         end
-        
+
         def self.db_time_now
           Time.now.utc
         end
-        
+
         def self.find_available(worker_name, limit = 5, max_run_time = Worker.max_run_time)
           right_now = db_time_now
 
           conditions = {:run_at  => {"$lte" => right_now}, :failed_at => nil}
           (conditions[:priority] ||= {})['$gte'] = Worker.min_priority.to_i if Worker.min_priority
           (conditions[:priority] ||= {})['$lte'] = Worker.max_priority.to_i if Worker.max_priority
-         
-          
+
+
           where = "this.locked_at == null || this.locked_at < #{make_date(right_now - max_run_time)}"
           results = self.where(conditions.merge(:locked_by => worker_name)).limit(-limit).order_by([['priority', 1], ['run_at', 1]]).to_a
           results += self.where(conditions.merge('$where' => where)).limit(-limit+results.size).order_by([['priority', 1], ['run_at', 1]]).to_a if results.size < limit
@@ -48,16 +47,16 @@ module Delayed
         def self.clear_locks!(worker_name)
           self.collection.update({:locked_by => worker_name}, {"$set" => {:locked_at => nil, :locked_by => nil}}, :multi => true)
         end
-        
+
         # Lock this job for this worker.
         # Returns true if we have the lock, false otherwise.
         def lock_exclusively!(max_run_time, worker = worker_name)
           right_now = self.class.db_time_now
           overtime = right_now - max_run_time.to_i
-          
+
           query = "this.locked_at == null || this.locked_at < #{make_date(overtime)} || this.locked_by == #{worker.to_json}"
           conditions = {:_id => id, :run_at => {"$lte" => right_now}, "$where" => query}
-          
+
           self.collection.update(conditions, {"$set" => {:locked_at => right_now, :locked_by => worker}})
           affected_rows = self.collection.find({:_id => id, :locked_by => worker}).count
           if affected_rows == 1
@@ -68,7 +67,7 @@ module Delayed
             return false
           end
         end
-        
+
         private
 
         def self.make_date(date_or_seconds)
