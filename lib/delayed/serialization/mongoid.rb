d8bcee29520877be2a82a8264ddfbaaf6386d566
Brandon Keepers <brandon@collectiveidea.com>
2010-11-14 22:36:45 -0600

    Fix failing serialization test

--- lib/delayed/serialization/mongoid.rb
+++ lib/delayed/serialization/mongoid.rb
@@ -1,14 +1,14 @@
 Mongoid::Document.class_eval do
   yaml_as "tag:ruby.yaml.org,2002:Mongoid"
-  
+
   def self.yaml_new(klass, tag, val)
     begin
       klass.find(val['attributes']['_id'])
     rescue Mongoid::Errors::DocumentNotFound
-      nil
+      raise Delayed::DeserializationError
     end
   end
-  
+
   def to_yaml_properties
     ['@attributes']
   end
