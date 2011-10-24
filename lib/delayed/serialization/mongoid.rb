584b1ab831227b7e7c827f26cb24fffd72655e39
Brian Ryckbost <bryckbost@gmail.com>
2011-10-24 13:37:18 -0400

    Add a conditional to switch between syck and psych serializations

--- lib/delayed/serialization/mongoid.rb
+++ lib/delayed/serialization/mongoid.rb
@@ -1,17 +1,25 @@
 # encoding: utf-8
-Mongoid::Document.class_eval do
-  yaml_as "tag:ruby.yaml.org,2002:Mongoid"
+if YAML.parser.class.name =~ /syck/i
+  Mongoid::Document.class_eval do
+    yaml_as "tag:ruby.yaml.org,2002:Mongoid"
 
-  def self.yaml_new(klass, tag, val)
-    begin
-      klass.find(val['attributes']['_id'])
-    rescue Mongoid::Errors::DocumentNotFound
-      raise Delayed::DeserializationError
+    def self.yaml_new(klass, tag, val)
+      begin
+        klass.find(val['attributes']['_id'])
+      rescue Mongoid::Errors::DocumentNotFound
+        raise Delayed::DeserializationError
+      end
+    end
+
+    def to_yaml_properties
+      ['@attributes']
     end
   end
-
-  def to_yaml_properties
-    ['@attributes']
+else
+  Mongoid::Document.class_eval do
+    def encode_with(coder)
+      coder["attributes"] = @attributes
+      coder.tag = ['!ruby/Mongoid', self.class.name].join(':')
+    end
   end
 end
-
