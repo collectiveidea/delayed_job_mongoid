ab406a4d5cd6c413a6f7682a8fe9e99d14b5abd3
Alexey Savartsov <asavartsov@gmail.com>
2012-04-16 02:53:33 +0400

    finder fix in Job#reverse

--- lib/delayed/backend/mongoid.rb
+++ lib/delayed/backend/mongoid.rb
@@ -60,7 +60,7 @@ module Delayed
 
             # Return result as a Mongoid document.
             # When Mongoid starts supporting findAndModify, this extra step should no longer be necessary.
-            self.find(:first, :conditions => {:_id => result["_id"]}) unless result.nil?
+            self.find(result["_id"]) unless result.nil?
           rescue Mongo::OperationFailure
             nil # no jobs available
           end
