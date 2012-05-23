b24f871849b34cc9e9cf86e2e26cfbeeb21a45ce
Alexey Savartsov <asavartsov@gmail.com>
2012-05-23 08:34:59 +0400

    Mongoid 3 support

--- lib/delayed/backend/mongoid.rb
+++ lib/delayed/backend/mongoid.rb
@@ -24,14 +24,6 @@ module Delayed
 
         before_save :set_default_run_at
 
-        def self.before_fork
-          ::Mongoid.master.connection.close
-        end
-
-        def self.after_fork
-          ::Mongoid.master.connection.connect
-        end
-
         def self.db_time_now
           Time.now.utc
         end
@@ -39,40 +31,31 @@ module Delayed
         # Reserves this job for the worker.
         #
         # Uses Mongo's findAndModify operation to atomically pick and lock one
-        # job from from the collection. findAndModify is not yet available
-        # directly thru Mongoid so go down to the Mongo Ruby driver instead.
+        # job from from the collection. 
         def self.reserve(worker, max_run_time = Worker.max_run_time)
           right_now = db_time_now
 
-          conditions = {:run_at  => {"$lte" => right_now}, :failed_at => nil}
-          (conditions[:priority] ||= {})['$gte'] = Worker.min_priority.to_i if Worker.min_priority
-          (conditions[:priority] ||= {})['$lte'] = Worker.max_priority.to_i if Worker.max_priority
-          (conditions[:queue] ||= {})['$in'] = Worker.queues if Worker.queues.any?
-
-          conditions['$or'] = [
+          criteria = self.where(
+            :run_at => {"$lte" => right_now}, 
+            :failed_at => nil
+          ).any_of(
             { :locked_by => worker.name },
             { :locked_at => nil },
             { :locked_at => { '$lt' => (right_now - max_run_time) }}
-          ]
+          )
 
-          begin
-            result = self.db.collection(self.collection.name).find_and_modify(
-              :query  => conditions,
-              :sort   => [['locked_by', -1], ['priority', 1], ['run_at', 1]],
-              :update => {"$set" => {:locked_at => right_now, :locked_by => worker.name}}
-            )
+          criteria = criteria.where(:priority => {"$gte" => Worker.min_priority.to_i}) if Worker.min_priority
+          criteria = criteria.where(:priority => {"$lte" => Worker.max_priority.to_i}) if Worker.max_priority
+          criteria = criteria.any_in(:queue => Worker.queues) if Worker.queues.any?
 
-            # Return result as a Mongoid document.
-            # When Mongoid starts supporting findAndModify, this extra step should no longer be necessary.
-            self.find(result["_id"]) unless result.nil?
-          rescue Mongo::OperationFailure
-            nil # no jobs available
-          end
+          criteria.desc(:locked_by).asc(:priority).asc(:run_at).find_and_modify(
+            "$set" => {:locked_at => right_now, :locked_by => worker.name}
+          )
         end
 
         # When a worker is exiting, make sure we don't have any locked jobs.
         def self.clear_locks!(worker_name)
-          self.collection.update({:locked_by => worker_name}, {"$set" => {:locked_at => nil, :locked_by => nil}}, :multi => true)
+          self.where(:locked_by => worker_name).update_all({:locked_at => nil, :locked_by => nil})
         end
 
         def reload(*args)
