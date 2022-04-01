a61083afd1b1fdb3d381e28465359a6ac29ccdf0
johnnyshields <johnny.shields@gmail.com>
2022-04-01 20:18:42 +0900

    Add RSpec

--- spec/delayed_job_mongoid_spec.rb
+++ spec/delayed_job_mongoid_spec.rb
@@ -7,7 +7,7 @@ describe Delayed::Backend::Mongoid::Job do
 
   context 'when job contains two instance variables that reference the same Mongoid document' do
     it 'can serialize and deserialize the job' do
-      job = Delayed::Job.create payload_object: StoryWrapperJob.new
+      job = Delayed::Job.create(payload_object: StoryWrapperJob.new)
       expect(job.reload.payload_object).to be_a StoryWrapperJob
     end
   end
@@ -27,6 +27,26 @@ describe Delayed::Backend::Mongoid::Job do
       expect_any_instance_of(::Mongo::Client).to receive(:close)
       expect_any_instance_of(::Mongo::Client).to receive(:reconnect)
       Delayed::Job.after_fork
+    end
+  end
+
+  context 'custom job class' do
+    let(:custom_job_class) do
+      Class.new do
+        include ::Mongoid::Document
+        store_in(collection: :my_jobs)
+        include ::Delayed::Backend::Mongoid::Mixin
+      end
+    end
+
+    it 'can use a custom job class' do
+      expect(custom_job_class.collection.name).to eq 'my_jobs'
+      expect(::Delayed::Job.all.count).to eq 0
+      expect(custom_job_class.all.count).to eq 0
+      custom_job_class.enqueue(StoryWrapperJob.new)
+      expect(::Delayed::Job.all.count).to eq 0
+      expect(custom_job_class.count).to eq 1
+      expect(custom_job_class.all.first.reload.payload_object).to be_a StoryWrapperJob
     end
   end
 end
